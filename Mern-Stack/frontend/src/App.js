130ed4fd87910f83195f9581300c7bd259fade07
pujaribalaji <pujaribalaji152@gmail.com>
2023-08-02 17:08:27 +0530

    first frontend code -LOGIN.js

--- Mern-Stack/frontend/src/App.js
+++ Mern-Stack/frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Login from './components/auth/login';
 
 
 function App() {
   return (
     <div>
-      Hello World!
+      <Login />
     </div>
   );
 }
---
+++ Mern-Stack/frontend/src/components/auth/login.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./login.css";
+
+const Login = () => {
+  return (
+    <>
+      <div className="login">
+        <div className="text0">Sign in</div>
+        <div className="text1">Login to manage your account</div>
+        <div className="label1">
+          <form>
+            <input className="input1" type="email" placeholder="Email"></input>
+            <input
+              className="input2"
+              type="password"
+              placeholder="Password"
+            ></input>
+          </form>
+        </div>
+        <div className="text2">Forgot password?</div>
+        <div>
+          <button className="button">Sign in</button>
+        </div>
+        <div className="line3">
+          <hr className="line0" />
+          <div className="line1">or</div>
+          <hr className="line2" />
+        </div>
+        <div>
+          <a>
+            <button className="button1">Sign in with Google</button>
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
